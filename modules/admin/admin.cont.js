bd9dc817148fa8dce796ee274ba169305957aff2
Treejackky <70340189+Treejackky@users.noreply.github.com>
2025-06-06 01:08:57 +0700

    add admin find all

--- modules/admin/admin.cont.js
+++ modules/admin/admin.cont.js
@@ -2,9 +2,10 @@ const { Admin } = require('../../db');
 
 exports.findAdmin = async (req, res) => {
     try {
-        // const admin = await Admin.findAll();
+        const admin = await Admin.findAll();
         res.status(200).json({
             message: 'Admin found',
+            data: admin
         });
     } catch (error) {
         res.status(500).json({ message: error.message });
--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,13 @@
 const [, , method] = process.argv;
-const { sequelize } = require('../db/index');
+const { db } = require('../db/index');
 
 exports.syncDB = async ({ alter = false }) => {
     try{
         console.log(`Syncing db -> ${process.env.DB_NAME} ${alter ? 'alter' : 'force'}`);
         if(alter){
-            await sequelize.sync({ alter: true });
+            await db.sync({ alter: true });
         }else{
-            await sequelize.sync({ force: true });
+            await db.sync({ force: true });
         }
         console.log(`Syncing db -> ${process.env.DB_NAME} ${alter ? 'alter' : 'force'} success`);
     }catch(error){
@@ -16,10 +16,9 @@ exports.syncDB = async ({ alter = false }) => {
     }
 }
 
-
 // ให้ run syncDB
 if(method === 'sync-alter'){
-    this.syncDB({}).then(() => process.exit(0));
+    this.syncDB({ alter: true }).then(() => process.exit(0));
 }else if(method === 'sync-force'){
     this.syncDB({ force: true }).then(() => process.exit(0));
 }else{
