1bc818a76cabaed94fc64f8d6249883c37834459
Maoswagger <70340189+Treejackky@users.noreply.github.com>
2025-07-04 02:27:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Welcome API endpoint
 app.get('/api', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the API'
     });
 });
 
-// Other API routes
 app.use('/api', require('./router'));
 
 app.all('*', (req, res) => {
